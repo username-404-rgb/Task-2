dc03a0ce672e5bd82dd92dd9945ba5f8af6fb701
username-404-rgb <alexander.adams4@education.nsw.gov.au>
2025-05-20 15:06:41 +1000

    Added code comments and the 'Card' class
    Need to actually move onto sprint two content

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 
 def main():
-    print('')
+    print('')                   # Prints a main menu for the player
     print('Start Game ~ (1)')
     print('Make Card  ~ (2)')
     print('View Cards ~ (3)')
@@ -8,8 +8,8 @@ def main():
     print('')
     print('Decend The Dungeon Depths, and Return With Pride')
     print('')
-    input1 = input('?')
-    if input1 == '1':
+    input1 = input('?')         # Lets the player input a choice
+    if input1 == '1':           # If they select 'Start Game', then allows them to chose difficulty
         print('')
         print('~Start Game~')
         print('Difficulty')
@@ -19,7 +19,7 @@ def main():
         print('')
         print('(X) Back')
         print('')
-        input2 = input('?').lower()
+        input2 = input('?').lower()     # Converts their input to lowercase for easier formatting of code
         if input2 == 'e':
             print('Easy Selected') # For Sprint 2
         elif input2 == 'n':
@@ -29,15 +29,15 @@ def main():
         elif input2 == 'x':
             main()
         else:
-            print('Try again')
+            print('Try again')          # If thier input isn't valid sends them back to the main menu, and lets them know they did something wrong
             main()
-    elif input1 == '2':
+    elif input1 == '2':                 # If their input is 'Make Card', runs that function
         print('Card Maker') # For Sprint 2
-    elif input1 == '3':
+    elif input1 == '3':                 # If their input is 'View Cards', runs that function
         print('View Cards') # For Sprint 2
-    elif input1 != '4':
-        print ('Error, Try Again')
+    elif input1 != '4':                 # If their input isn't 'Quit', lets them know they did something wrong and runs main again
+        print ('Error, Try Again')      
         main()
-    else:
+    else:                               # If their input is 'Quit', ends program
         print('')
 main()
---
+++ dungeon_game.py
@@ -0,0 +1,6 @@
+class Card:
+	def __init__(self, name, suit, cost):
+		self.name = name
+		self.suit = suit
+		self.cost = cost
+
