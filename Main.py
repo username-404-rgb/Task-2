420b09b8ce24ddb971dff0a30010117cda69dd83
username-404-rgb <alexander.adams4@education.nsw.gov.au>
2025-05-22 15:09:44 +1000

    Restructuring for project for future sprints
    Changed to a text adventure to make programming easier for this project.
    Hoping to use classes for storing items in the player's inventory

--- Main.py
+++ Main.py
@@ -1,43 +1,28 @@
+def left_path():
+    print('You go down the bright path')
+
+def right_path():
+    print('You take your chances in the darkness, and find a sign\n'
+          'In the darkness you cannot read it, and you ignore it for now\n'
+          'Continuing, you hear a low noise, far away right now, you wonder if you should\n'
+          '1 - Continue forward with apprehension, or\n'
+          '2 - Turn around, and venture down the other path\n')
+
 
 def main():
-    print('')                   # Prints a main menu for the player
-    print('Start Game ~ (1)')
-    print('Make Card  ~ (2)')
-    print('View Cards ~ (3)')
-    print('Quit       ~ (4)')
-    print('')
-    print('Decend The Dungeon Depths, and Return With Pride')
-    print('')
-    input1 = input('?')         # Lets the player input a choice
-    if input1 == '1':           # If they select 'Start Game', then allows them to chose difficulty
-        print('')
-        print('~Start Game~')
-        print('Difficulty')
-        print('(E)asy')
-        print('(N)ormal')
-        print('(H)ard')
-        print('')
-        print('(X) Back')
-        print('')
-        input2 = input('?').lower()     # Converts their input to lowercase for easier formatting of code
-        if input2 == 'e':
-            print('Easy Selected') # For Sprint 2
-        elif input2 == 'n':
-            print('Normal Selected') # For Sprint 2
-        elif input2 == 'h':
-            print('Hard Selected') # For Sprint 2
-        elif input2 == 'x':
-            main()
-        else:
-            print('Try again')          # If thier input isn't valid sends them back to the main menu, and lets them know they did something wrong
-            main()
-    elif input1 == '2':                 # If their input is 'Make Card', runs that function
-        print('Card Maker') # For Sprint 2
-    elif input1 == '3':                 # If their input is 'View Cards', runs that function
-        print('View Cards') # For Sprint 2
-    elif input1 != '4':                 # If their input isn't 'Quit', lets them know they did something wrong and runs main again
-        print ('Error, Try Again')      
-        main()
-    else:                               # If their input is 'Quit', ends program
-        print('')
+    print('You wake up in a forest clearing, unsure of how you got here, ahead you see two paths\n'
+         'The left path is brightly lit with a lantern, the right is pitch black\n'
+         '1 - Proceed down the left path, or\n'
+         '2 - Venture down the unknown right path \n')
+
+    choice = int(input('Enter choice: '))
+    if choice == 1:
+        left_path()
+    elif choice == 2:
+        right_path()
+    elif choice == 9:
+        print('Game Over')
+    else:
+        print('Enter a valid number, silly!')
+
 main()
