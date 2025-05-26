b3d5c645fa2b13c8137d04c6d87ea18260ac86c1
username-404-rgb <alexander.adams4@education.nsw.gov.au>
2025-05-26 15:10:01 +1000

    Further Development
    More scenes, implemented Items

---
+++ Item_Class.py
@@ -0,0 +1,15 @@
+class Item:
+	def __init__(self, name, collected):
+		self.name = name
+		self.collected = collected
+
+	def get_collected(self):
+		return self.collected
+
+	def set_collected(self, new_name):
+		if isinstance(new_name, str):
+			self.collected = new_name
+		else:
+			raise ValueError("Name must be a string.")
+
+lantern = Item('lantern', 'false')
--- Text_adventure.py
+++
@@ -1,6 +0,0 @@
-class Card:
-	def __init__(self, name, suit, cost):
-		self.name = name
-		self.suit = suit
-		self.cost = cost
-
